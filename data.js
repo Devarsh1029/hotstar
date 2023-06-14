6aac800964de1c3c9feebfd54bf08542ebcd5602
Devarsh <71457920+Devarsh1029@users.noreply.github.com>
2023-06-14 12:15:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    
+    {
+        name: 'Loki',
+        des: 'The mercurial villian Loki resumes his role as the God of Mischief in a new series that takes place after the events of "Avengers:Endgame."',
+        image: 'slider 1.png'
+    },
+    {
+        name: 'Falcon And The Winter Soldier',
+        des: 'Following the events of "Avengers:Endgame", Sam Wilson andBucky Barnes team up in a global adventure that test their abilities and their patience.',
+        image: 'slider 2.png'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Wanda Maximoff and Vision-two super-powered beings living idealized suburban lives-begin to suspect that everything is not as it seems.',
+        image: 'slider 3.png'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.',
+        image: 'slider 4.png'
+    },
+    {
+        name: 'luca',
+        des: 'The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.',
+        image: 'slider 5.png'
+    }
+]
